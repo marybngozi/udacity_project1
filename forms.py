4d032477b91e12bedf9a5ae3320ff9bc8d7379c8
marybngozi <umeh.mary@appmartgroup.com>
2022-04-21 22:25:12 +0100

    feat: add models as in TODOs

--- forms.py
+++ forms.py
@@ -1,8 +1,95 @@
 from datetime import datetime
+from email.policy import default
 from flask_wtf import Form
 from wtforms import StringField, SelectField, SelectMultipleField, DateTimeField, BooleanField
 from wtforms.validators import DataRequired, AnyOf, URL
 
+genre_choices = [
+    ('Alternative', 'Alternative'),
+    ('Blues', 'Blues'),
+    ('Classical', 'Classical'),
+    ('Country', 'Country'),
+    ('Electronic', 'Electronic'),
+    ('Folk', 'Folk'),
+    ('Funk', 'Funk'),
+    ('Hip-Hop', 'Hip-Hop'),
+    ('Heavy Metal', 'Heavy Metal'),
+    ('Instrumental', 'Instrumental'),
+    ('Jazz', 'Jazz'),
+    ('Musical Theatre', 'Musical Theatre'),
+    ('Pop', 'Pop'),
+    ('Punk', 'Punk'),
+    ('R&B', 'R&B'),
+    ('Reggae', 'Reggae'),
+    ('Rock n Roll', 'Rock n Roll'),
+    ('Soul', 'Soul'),
+    ('Other', 'Other'),
+]
+
+state_choices = [
+    ('AL', 'AL'),
+    ('AK', 'AK'),
+    ('AZ', 'AZ'),
+    ('AR', 'AR'),
+    ('CA', 'CA'),
+    ('CO', 'CO'),
+    ('CT', 'CT'),
+    ('DE', 'DE'),
+    ('DC', 'DC'),
+    ('FL', 'FL'),
+    ('GA', 'GA'),
+    ('HI', 'HI'),
+    ('ID', 'ID'),
+    ('IL', 'IL'),
+    ('IN', 'IN'),
+    ('IA', 'IA'),
+    ('KS', 'KS'),
+    ('KY', 'KY'),
+    ('LA', 'LA'),
+    ('ME', 'ME'),
+    ('MT', 'MT'),
+    ('NE', 'NE'),
+    ('NV', 'NV'),
+    ('NH', 'NH'),
+    ('NJ', 'NJ'),
+    ('NM', 'NM'),
+    ('NY', 'NY'),
+    ('NC', 'NC'),
+    ('ND', 'ND'),
+    ('OH', 'OH'),
+    ('OK', 'OK'),
+    ('OR', 'OR'),
+    ('MD', 'MD'),
+    ('MA', 'MA'),
+    ('MI', 'MI'),
+    ('MN', 'MN'),
+    ('MS', 'MS'),
+    ('MO', 'MO'),
+    ('PA', 'PA'),
+    ('RI', 'RI'),
+    ('SC', 'SC'),
+    ('SD', 'SD'),
+    ('TN', 'TN'),
+    ('TX', 'TX'),
+    ('UT', 'UT'),
+    ('VT', 'VT'),
+    ('VA', 'VA'),
+    ('WA', 'WA'),
+    ('WV', 'WV'),
+    ('WI', 'WI'),
+    ('WY', 'WY'),
+]
+
+# def validate_genres(form, field):
+#         genres_values = [choice[1] for choice in genre_choices]
+#         for value in field.data:
+#             if value not in genres_values:
+#                 raise ValidationError('Invalid genres value')
+
+# def validate_phone(form, field):
+#     if not re.search(r"^[0-9]{3}-[0-9]{3}-[0-9]{4}$", field.data):
+#         raise ValidationError("Invalid phone number")
+        
 class ShowForm(Form):
     artist_id = StringField(
         'artist_id'
@@ -25,59 +112,7 @@ class VenueForm(Form):
     )
     state = SelectField(
         'state', validators=[DataRequired()],
-        choices=[
-            ('AL', 'AL'),
-            ('AK', 'AK'),
-            ('AZ', 'AZ'),
-            ('AR', 'AR'),
-            ('CA', 'CA'),
-            ('CO', 'CO'),
-            ('CT', 'CT'),
-            ('DE', 'DE'),
-            ('DC', 'DC'),
-            ('FL', 'FL'),
-            ('GA', 'GA'),
-            ('HI', 'HI'),
-            ('ID', 'ID'),
-            ('IL', 'IL'),
-            ('IN', 'IN'),
-            ('IA', 'IA'),
-            ('KS', 'KS'),
-            ('KY', 'KY'),
-            ('LA', 'LA'),
-            ('ME', 'ME'),
-            ('MT', 'MT'),
-            ('NE', 'NE'),
-            ('NV', 'NV'),
-            ('NH', 'NH'),
-            ('NJ', 'NJ'),
-            ('NM', 'NM'),
-            ('NY', 'NY'),
-            ('NC', 'NC'),
-            ('ND', 'ND'),
-            ('OH', 'OH'),
-            ('OK', 'OK'),
-            ('OR', 'OR'),
-            ('MD', 'MD'),
-            ('MA', 'MA'),
-            ('MI', 'MI'),
-            ('MN', 'MN'),
-            ('MS', 'MS'),
-            ('MO', 'MO'),
-            ('PA', 'PA'),
-            ('RI', 'RI'),
-            ('SC', 'SC'),
-            ('SD', 'SD'),
-            ('TN', 'TN'),
-            ('TX', 'TX'),
-            ('UT', 'UT'),
-            ('VT', 'VT'),
-            ('VA', 'VA'),
-            ('WA', 'WA'),
-            ('WV', 'WV'),
-            ('WI', 'WI'),
-            ('WY', 'WY'),
-        ]
+        choices= state_choices
     )
     address = StringField(
         'address', validators=[DataRequired()]
@@ -89,29 +124,9 @@ class VenueForm(Form):
         'image_link'
     )
     genres = SelectMultipleField(
-        # TODO implement enum restriction
+        # TODO: implement enum restriction
         'genres', validators=[DataRequired()],
-        choices=[
-            ('Alternative', 'Alternative'),
-            ('Blues', 'Blues'),
-            ('Classical', 'Classical'),
-            ('Country', 'Country'),
-            ('Electronic', 'Electronic'),
-            ('Folk', 'Folk'),
-            ('Funk', 'Funk'),
-            ('Hip-Hop', 'Hip-Hop'),
-            ('Heavy Metal', 'Heavy Metal'),
-            ('Instrumental', 'Instrumental'),
-            ('Jazz', 'Jazz'),
-            ('Musical Theatre', 'Musical Theatre'),
-            ('Pop', 'Pop'),
-            ('Punk', 'Punk'),
-            ('R&B', 'R&B'),
-            ('Reggae', 'Reggae'),
-            ('Rock n Roll', 'Rock n Roll'),
-            ('Soul', 'Soul'),
-            ('Other', 'Other'),
-        ]
+        choices=genre_choices
     )
     facebook_link = StringField(
         'facebook_link', validators=[URL()]
@@ -119,9 +134,7 @@ class VenueForm(Form):
     website_link = StringField(
         'website_link'
     )
-
-    seeking_talent = BooleanField( 'seeking_talent' )
-
+    seeking_talent = BooleanField( 'seeking_talent', default=False )
     seeking_description = StringField(
         'seeking_description'
     )
@@ -137,62 +150,10 @@ class ArtistForm(Form):
     )
     state = SelectField(
         'state', validators=[DataRequired()],
-        choices=[
-            ('AL', 'AL'),
-            ('AK', 'AK'),
-            ('AZ', 'AZ'),
-            ('AR', 'AR'),
-            ('CA', 'CA'),
-            ('CO', 'CO'),
-            ('CT', 'CT'),
-            ('DE', 'DE'),
-            ('DC', 'DC'),
-            ('FL', 'FL'),
-            ('GA', 'GA'),
-            ('HI', 'HI'),
-            ('ID', 'ID'),
-            ('IL', 'IL'),
-            ('IN', 'IN'),
-            ('IA', 'IA'),
-            ('KS', 'KS'),
-            ('KY', 'KY'),
-            ('LA', 'LA'),
-            ('ME', 'ME'),
-            ('MT', 'MT'),
-            ('NE', 'NE'),
-            ('NV', 'NV'),
-            ('NH', 'NH'),
-            ('NJ', 'NJ'),
-            ('NM', 'NM'),
-            ('NY', 'NY'),
-            ('NC', 'NC'),
-            ('ND', 'ND'),
-            ('OH', 'OH'),
-            ('OK', 'OK'),
-            ('OR', 'OR'),
-            ('MD', 'MD'),
-            ('MA', 'MA'),
-            ('MI', 'MI'),
-            ('MN', 'MN'),
-            ('MS', 'MS'),
-            ('MO', 'MO'),
-            ('PA', 'PA'),
-            ('RI', 'RI'),
-            ('SC', 'SC'),
-            ('SD', 'SD'),
-            ('TN', 'TN'),
-            ('TX', 'TX'),
-            ('UT', 'UT'),
-            ('VT', 'VT'),
-            ('VA', 'VA'),
-            ('WA', 'WA'),
-            ('WV', 'WV'),
-            ('WI', 'WI'),
-            ('WY', 'WY'),
-        ]
+        choices= state_choices
     )
     phone = StringField(
-        # TODO implement validation logic for state
+        # TODO: implement validation logic for state
         'phone'
     )
     image_link = StringField(
@@ -200,40 +161,17 @@ class ArtistForm(Form):
     )
     genres = SelectMultipleField(
         'genres', validators=[DataRequired()],
-        choices=[
-            ('Alternative', 'Alternative'),
-            ('Blues', 'Blues'),
-            ('Classical', 'Classical'),
-            ('Country', 'Country'),
-            ('Electronic', 'Electronic'),
-            ('Folk', 'Folk'),
-            ('Funk', 'Funk'),
-            ('Hip-Hop', 'Hip-Hop'),
-            ('Heavy Metal', 'Heavy Metal'),
-            ('Instrumental', 'Instrumental'),
-            ('Jazz', 'Jazz'),
-            ('Musical Theatre', 'Musical Theatre'),
-            ('Pop', 'Pop'),
-            ('Punk', 'Punk'),
-            ('R&B', 'R&B'),
-            ('Reggae', 'Reggae'),
-            ('Rock n Roll', 'Rock n Roll'),
-            ('Soul', 'Soul'),
-            ('Other', 'Other'),
-        ]
-     )
+        choices=genre_choices
+    )
     facebook_link = StringField(
-        # TODO implement enum restriction
+        # TODO: implement enum restriction
         'facebook_link', validators=[URL()]
-     )
-
+    )
     website_link = StringField(
         'website_link'
-     )
-
+    )
     seeking_venue = BooleanField( 'seeking_venue' )
-
     seeking_description = StringField(
-            'seeking_description'
-     )
+        'seeking_description'
+    )
 
--- models.py
+++ models.py
@@ -1,34 +1,21 @@
 from flask_sqlalchemy import SQLAlchemy
-db = SQLAlchemy()
-import enum
+from datetime import datetime
 
-# enum for genres
-class Genre(enum.Enum):
-  Alternative = 'Alternative'
-  Blues = 'Blues'
-  Classical = 'Classical'
-  Country = 'Country'
-  Electronic = 'Electronic'
-  Folk = 'Folk'
-  Funk = 'Funk'
-  HipHop = 'Hip-Hop'
-  HeavyMetal = 'Heavy Metal'
-  Instrumental = 'Instrumental'
-  Jazz = 'Jazz'
-  MusicalTheatre = 'Musical Theatre'
-  Pop = 'Pop'
-  Punk = 'Punk'
-  RnB = 'R&B'
-  Reggae = 'Reggae'
-  RocknRoll = 'Rock n Roll'
-  Soul = 'Soul'
-  Other = 'Other'
+db = SQLAlchemy()
 
 #----------------------------------------------------------------------------#
 # Models.
 #----------------------------------------------------------------------------#
+class Show(db.Model):
+  __tablename__ = 'show'
+
+  id = db.Column(db.Integer, primary_key=True)
+  start_time = db.Column(db.DateTime, nullable=False)
+  artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'), nullable=False)
+  venue_id = db.Column(db.Integer, db.ForeignKey('venue.id'), nullable=False)
+
 class Venue(db.Model):
-  __tablename__ = 'Venue'
+  __tablename__ = 'venue'
 
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String)
@@ -38,33 +25,75 @@ class Venue(db.Model):
   phone = db.Column(db.String(120))
   image_link = db.Column(db.String(500))
   facebook_link = db.Column(db.String(120))
-  genres = db.Column(db.Enum(Genre))
-  website_link = db.Column(db.String(120))
+  genres = db.Column(db.ARRAY(db.String()))
+  website_link = db.Column(db.String(500))
   seeking_talent = db.Column(db.Boolean, default=False)
   seeking_description = db.Column(db.Text)
+  shows = db.relationship('Show', backref='venue', lazy=True, cascade='all, delete-orphan')
+  created_on = db.Column(db.DateTime, default=datetime.utcnow)
 
-  # TODO: implement any missing fields, as a database migration using
+  @property 
+  def upcoming_shows(self):
+    upcoming_shows = [show for show in self.shows if show.start_time > datetime.now()]
+    return upcoming_shows
+  
+  @property
+  def num_upcoming_shows(self):
+    return len(self.upcoming_shows)
+  
+  @property
+  def past_shows(self):
+    past_shows = [show for show in self.shows if show.start_time < datetime.now()]
+    return past_shows
+  
+  @property
+  def num_past_shows(self):
+    return len(self.past_shows)
+
+
+  # implement any missing fields, as a database migration using
   # Flask-Migrate
 
 
 class Artist(db.Model):
-  __tablename__ = 'Artist'
+  __tablename__ = 'artist'
 
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String)
   city = db.Column(db.String(120))
   state = db.Column(db.String(120))
   phone = db.Column(db.String(120))
-  genres = db.Column(db.String(120))
+  genres = db.Column(db.ARRAY(db.String(120)))
   image_link = db.Column(db.String(500))
   facebook_link = db.Column(db.String(120))
+  website_link = db.Column(db.String(120))
+  seeking_venue = db.Column(db.Boolean, default=False)
+  seeking_description = db.Column(db.Text)
+  shows = db.relationship('Show', backref='artist', lazy=True)
+  created_on = db.Column(db.DateTime, default=datetime.utcnow)
 
-  # TODO: implement any missing fields, as a database migration using
+  @property
+  def upcoming_shows(self):
+    upcoming_shows = [show for show in self.shows if show.start_time > datetime.now()]
+    return upcoming_shows
+    
+  @property
+  def num_upcoming_shows(self):      
+    return len(self.upcoming_shows)
+      
+  @property
+  def past_shows(self):
+    past_shows = [show for show in self.shows if show.start_time < datetime.now()]
+    
+    return past_shows
+    
+  @property
+  def num_past_shows(self):
+    return len(self.past_shows)
+
+
+  # implement any missing fields, as a database migration using
   # Flask-Migrate
 
-class Show(db.Model):
-  __tablename__ = 'Show'
-  id = db.Column(db.Integer, primary_key=True)
-
-# TODO Implement Show and Artist models, and complete all model
+# Implement Show and Artist models, and complete all model
 # relationships and properties, as a database migration.
