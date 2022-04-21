332e93fdde9d3cf895e396f53d253bedb7bba7e4
marybngozi <umeh.mary@appmartgroup.com>
2022-04-21 22:26:08 +0100

    feat: add venue delete

--- static/js/script.js
+++ static/js/script.js
@@ -2,3 +2,29 @@ window.parseISOString = function parseISOString(s) {
   var b = s.split(/\D+/);
   return new Date(Date.UTC(b[0], --b[1], b[2], b[3], b[4], b[5], b[6]));
 };
+
+// delete a venue
+document.querySelector("#delete_venue").addEventListener("click", (e) => {
+  const venueId = e.target.dataset["id"];
+
+  // verify if the delete was intentional
+  if (
+    !confirm(
+      "Are you sure you want to delete this Venue and its associated Shows?"
+    )
+  ) {
+    return;
+  }
+
+  fetch(`/venues/${venueId}`, {
+    method: "DELETE",
+  })
+    .then((res) => {
+      console.log(res);
+      // go to the homepage
+      window.location = "/";
+    })
+    .catch((err) => {
+      console.log({ err });
+    });
+});
